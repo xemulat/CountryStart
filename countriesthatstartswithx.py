6f6697b64db8eaa88ab12a11c3c76e9ea8442f6e
xemulat <98595166+xemulat@users.noreply.github.com>
2022-08-11 17:46:37 +0200

    Add files via upload

---
+++ countriesthatstartswithx.py
@@ -0,0 +1,14 @@
+from webbrowser import open as webopen
+
+print("   _____                  _               _____ _             _          __ \n"
+"  / ____|                | |             / ____| |           | |        /_ |\n"
+" | |     ___  _   _ _ __ | |_ _ __ _   _| (___ | |_ __ _ _ __| |_  __   _| |\n"
+" | |    / _ \| | | | '_ \| __| '__| | | |\___ \| __/ _` | '__| __| \ \ / / |\n"
+" | |___| (_) | |_| | | | | |_| |  | |_| |____) | || (_| | |  | |_   \ V /| |\n"
+"  \_____\___/ \__,_|_| |_|\__|_|   \__, |_____/ \__\__,_|_|   \__|   \_/ |_|\n"
+"                                    __/ |                                   \n"
+"                                   |___/                                    \n")
+print()
+x = input("Input your First Letter: ")
+webopen("https://www.worldatlas.com/articles/countries-that-start-with-the-letter-" + x + ".html")
+print("Thanks for using my tool, made By Xemulated for Robuk free legit download")
